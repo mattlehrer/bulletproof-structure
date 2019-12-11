22a401d0683edbf057d3e7f0e542fac27f804636
Matt Lehrer <lehrerm@gmail.com>
2019-12-11 10:34:09 -0800

    extend ConnectionOptions interface

--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -3,9 +3,14 @@ import { Db } from 'mongodb';
 import config from '../config';
 
 export default async (): Promise<Db> => {
+  interface NewConnectionOptions extends mongoose.ConnectionOptions {
+    useUnifiedTopology?: boolean;
+  }
+
   const connection = await mongoose.connect(config.databaseURL, {
     useNewUrlParser: true,
     useCreateIndex: true,
-  });
+    useUnifiedTopology: true,
+  } as NewConnectionOptions);
   return connection.connection.db;
 };
